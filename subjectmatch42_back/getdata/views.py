752d159fe5a1c33392624654d2f650aa0f1eaea6
jbok <jbok@student.32seoul.kr>
2023-03-14 00:30:25 +0900

    implement get token and api

--- subjectmatch42_back/getdata/views.py
+++ subjectmatch42_back/getdata/views.py
@@ -6,19 +6,22 @@ import requests
 import time
 
 class	GetDataView(View):
+	id = 'u-s4t2ud-704d2685a6d5772b24b1c01b713439a29f2ebc33f8ec8ac99d27305213871b3c'
+	secret = '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'
 	def get(self, request):
-		id = 'u-s4t2ud-704d2685a6d5772b24b1c01b713439a29f2ebc33f8ec8ac99d27305213871b3c'
-		secret = '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'
-		code = str(request.GET.get('code', None))
+		code = request.GET.get('code', None)
 		if code == None:
 			return HttpResponse('error!')
-		data = {'grant_type': 'client_credentials', 'client_id': id,'client_secret': secret}
+		data = {'grant_type': 'authorization_code', 'client_id': self.id,'client_secret': self.secret, 'code': code, 'redirect_uri': 'http://localhost:8000/code', 'scope': 'public'}
 		print(data)
 		token = requests.post('https://api.intra.42.fr/v2/oauth/token', data=data)
 		if token == None:
 			return HttpResponse('error!')
-		print(token.text)
-		time.sleep(1)
-		res = requests.get("https://api.intra.42.fr/v2/users/", headers={'Authorization': 'Bearer ' + "access_token" + "d56fbf336f8253a8ffcef3d798ccdd639b3fc70f2140cfe91da004f2d24317c9"})
-		print(res)
-		return HttpResponse(token)
+		res = requests.get("https://api.intra.42.fr/v2/users/131690/projects_users", headers={'Authorization': 'Bearer ' + token.json()['access_token']})
+		if res == None:
+			return HttpResponse('error!')
+		res_d = res.json()
+		print("yooh's projects")
+		for projects in res.json():
+			print(f'project_name: {projects["project"]["name"]}, final_mark: {projects["teams"][-1]["final_mark"]}', end='\n')
+		return HttpResponse(res)
