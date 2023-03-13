59930f8bd0ee8be4184d1a4f17d8bf0a65264f58
Junseok Bok <jbok@c8r2s14.42seoul.kr>
2023-03-13 22:23:05 +0900

    s

--- subjectmatch42_back/getdata/views.py
+++ subjectmatch42_back/getdata/views.py
@@ -3,18 +3,22 @@ from django.http import HttpResponse
 from django.views import View
 import json
 import requests
+import time
 
 class	GetDataView(View):
-	id = 'u-s4t2ud-704d2685a6d5772b24b1c01b713439a29f2ebc33f8ec8ac99d27305213871b3c'
-	secret = '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'
 	def get(self, request):
-		code = request.GET.get('code', None)
+		id = 'u-s4t2ud-704d2685a6d5772b24b1c01b713439a29f2ebc33f8ec8ac99d27305213871b3c'
+		secret = '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'
+		code = str(request.GET.get('code', None))
 		if code == None:
 			return HttpResponse('error!')
-		data = json.dumps({"grant_type": "authorization_code", "client_id": self.id, "client_secret": self.secret, 'code': code})
-		token = requests.post('https://api.intra.42.fr/oauth/authorize', data)
+		data = {'grant_type': 'client_credentials', 'client_id': id,'client_secret': secret}
+		print(data)
+		token = requests.post('https://api.intra.42.fr/v2/oauth/token', data=data)
 		if token == None:
 			return HttpResponse('error!')
-		
-		print(token)
-		return HttpResponse(code)
+		print(token.text)
+		time.sleep(1)
+		res = requests.get("https://api.intra.42.fr/v2/users/", headers={'Authorization': 'Bearer ' + "access_token" + "d56fbf336f8253a8ffcef3d798ccdd639b3fc70f2140cfe91da004f2d24317c9"})
+		print(res)
+		return HttpResponse(token)
