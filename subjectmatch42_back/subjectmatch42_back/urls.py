677d0d0564bfca68d9f54c56d51d4c525a10aab0
kyc1204 <dpcks5959@naver.com>
2023-03-14 11:40:13 +0900

    init template

--- subjectmatch42_back/subjectmatch42_back/urls.py
+++ subjectmatch42_back/subjectmatch42_back/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('getdata.urls')),
     path('code/', include('getdata.urls')),
     path('admin/', admin.site.urls),
 ]
